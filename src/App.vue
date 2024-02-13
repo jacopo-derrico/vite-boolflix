17adce798cdaa9eb05d0bf5191a80174a259bf66
Jacopo D'Errico <87384611+jacopo-derrico@users.noreply.github.com>
2024-02-13 10:34:42 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppComponent from './components/AppComponent.vue';
+
+import { store } from "./store";
+
+export default {
+    components: {
+        AppComponent
+    },
+}
+</script>
+
+
+<template>
+<AppComponent/>
+</template>
+
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppComponent.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from "../store";
+
+export default {
+    name: 'AppComponent',
+    data() {
+        return {
+            store
+        };
+    }
+}
+</script>
+
+
+<template>
+
+</template>
+
+
+<style lang="scss" scoped>
+@use "./styles/general.scss" as *;
+</style>
