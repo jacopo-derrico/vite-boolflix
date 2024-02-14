437ec1f56a8ad9dbbaab796c2edc9d04d755e1d2
jacopo-derrico <derricoj.design@gmail.com>
2024-02-14 16:40:23 +0100

    header style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,13 +11,71 @@ export default {
 }
 </script>
 
-
 <template>
-    <input type="text" placeholder="Binge something..." v-model="store.searchMovie">
-    <button type="button" @click.prevent="$emit('performSearch')">Cerca</button>
+    <header class="flex align-center justify-between">
+        <nav class="flex align-center justify-between col-8 m-center">
+            <div>
+                <h1>BoolFlix</h1>
+            </div>
+            <div id="searchBar" class="flex align-center justify-end">
+                <div class="icon flex align-center justify-center">
+                    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                </div>
+                <input type="text" id="searchFilm" placeholder="Binge something..." v-model="store.searchMovie"
+                    @input="$emit('performSearch')">
+                <!-- <button type="button" @click.prevent="$emit('performSearch')">Cerca</button> -->
+            </div>
+        </nav>
+    </header>
 </template>
 
 
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
+
+header {
+    // background-color: #ffffff6d;
+    padding-block: 10px;
+
+    h1 {
+        color: white;
+    }
+
+    #searchBar {
+        width: 70%;
+    }
+}
+
+.icon {
+    padding-right: 15px;
+    color: #ffffff6d;
+    background-color: #121212;
+    border: 1px solid #ffffff44;
+    border-right: none;
+    border-radius: 5px 0 0 5px;
+    height: 30px;
+    padding-left: 10px;
+}
+
+input {
+    background-color: #121212;
+    color: white;
+    border: 1px solid #ffffff44;
+    border-radius: 0 5px 5px 0;
+    padding-left: 10px;
+    height: 30px;
+    width: 140px;
+    transition: width 0.6s ease-in-out;
+
+    &:focus {
+        width: 50%;
+        outline: none;
+    }
+}
+
+// .icon:active input {
+//     // outline: none;
+//     width: 100%;
+//     color: red;
+// }
 </style>
