40cce097adab9f2cd60c4425090dd6430a34a2a1
jacopo-derrico <derricoj.design@gmail.com>
2024-02-14 18:04:52 +0100

    show tranding from API

--- src/App.vue
+++ src/App.vue
@@ -18,14 +18,27 @@ export default {
         getMovies() {
             store.loading = true;
 
-            axios
-                .get(`https://api.themoviedb.org/3/search/multi?api_key=8766d6b6b214fc613264d11608cc2b52&query=${store.searchMovie}`)
-                .then(res => {
-                    store.searchResult = res.data.results
-                    store.loading = false
-                    console.log(res.data.results)
-                })
+            if (store.searchMovie === '') {
+                axios
+                    .get(`https://api.themoviedb.org/3/trending/all/day?api_key=8766d6b6b214fc613264d11608cc2b52&language=it-IT`)
+                    .then(res => {
+                        store.searchResult = res.data.results
+                        store.loading = false
+                        console.log(res.data.results)
+                    })
+            } else {
+                axios
+                    .get(`https://api.themoviedb.org/3/search/multi?api_key=8766d6b6b214fc613264d11608cc2b52&query=${store.searchMovie}`)
+                    .then(res => {
+                        store.searchResult = res.data.results
+                        store.loading = false
+                        console.log(res.data.results)
+                    })
+            }
         }
+    },
+    mounted() {
+        this.getMovies()
     },
 }
 </script>
