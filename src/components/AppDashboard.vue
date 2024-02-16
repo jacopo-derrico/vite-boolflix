d2649cf82e6db7f8d960802065445ce4525331f1
jacopo-derrico <derricoj.design@gmail.com>
2024-02-16 17:43:24 +0100

    add overview on trailer & animation

--- src/components/AppDashboard.vue
+++ src/components/AppDashboard.vue
@@ -13,26 +13,40 @@ export default {
         };
     },
     methods: {
-        getTrailer(id, type) {
-            
+        getTrailer(id, type, title, overview) {
+
+            store.trailerInfo = { title: title, overview: overview };
+            console.log(store.trailerInfo);
             console.log(id, type);
-            store.showTrailer = true
+            store.showTrailer = true;
+
+            if (type === undefined) {
+                type = 'tv'
+            }
+            console.log(id, type);
             axios
                 .get(`https://api.themoviedb.org/3/${type}/${id}/videos?api_key=8766d6b6b214fc613264d11608cc2b52&language=it_IT"`)
                 .then(res => {
-                    res.data.results.forEach(link => {
-                        if (link.site == 'YouTube' && link.type == 'Teaser' && link.official) {
+                    res.data.results.some(link => {
+                        if (link.site == 'YouTube' && (link.type == 'Teaser' || link.type == 'Trailer')) {
                             store.teaserKey = link.key
-                            return
+                            return true
                         }
                     })
                 })
                 .catch(error => {
                     console.error('Error fetching trailers:', error);
                 });
-        }
+        },
+        cleanTrailer() {
+            store.showTrailer = false;
+            store.trailerInfo = {};
+            store.teaserKey = '';
+        },
+        mouseOverInfo(){
+            store.infoHover = !store.infoHover; 
     },
-}
+}}
 </script>
 
 <template>
@@ -40,13 +54,21 @@ export default {
         <AppMovieCard v-for="(movie, index) in store.searchResult" :key="index" :propsTitle="movie.title || movie.name"
             :propsOgTitle="movie.original_title || movie.original_name" :propsLang="movie.original_language"
             :propsScore="movie.vote_average" :propsImg="movie.poster_path" :propsScoreCount="movie.vote_count"
-            @click="getTrailer(movie.id, movie.media_type)" />
+            @click="getTrailer(movie.id, movie.media_type, (movie.title || movie.name), movie.overview)" />
         <div class="focusCard" :class="store.showTrailer ? 'visible' : 'hidden'">
-            <div class="info">
-                <font-awesome-icon icon="fa-solid fa-xmark" />
+            <font-awesome-icon icon="fa-solid fa-xmark" @click="cleanTrailer" />
+            <div class="info"
+                    @mouseover="mouseOverInfo"
+                    @mouseleave="mouseOverInfo">
                 <iframe type="text/html"
                     :src="`https://www.youtube.com/embed/${store.teaserKey}?autoplay=1&controls=0&disablekb=1&enablejsapi=1&fs=0&loop=1&modestbranding=1&iv_load_policy=3`"
                     frameborder="0"></iframe>
+                <div id="overlay" :class="store.infoHover ? 'visible' : 'hidden'">
+                    <div id="data" class="flex-column align-end">
+                        <h2>{{ store.trailerInfo.title }}</h2>
+                        <p>{{ store.trailerInfo.overview }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -73,16 +95,66 @@ export default {
     z-index: 99;
     background-color: #00000060;
 
+    .fa-xmark {
+        color: white;
+        position: absolute;
+        right: 8%;
+        top: 7%;
+        cursor: pointer;
+    }
+
     .info {
         width: 80%;
         height: 80%;
         margin: 5% auto;
+        position: relative;
     }
 
     iframe {
         object-fit: cover;
         width: 100%;
         height: 100%;
+    }
+
+    #overlay {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        bottom: 0;
+        left: 0;
+        height: 30%;
+        background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%);
+    }
+
+    #data {
+        color: white;
+        padding: 30px;
+        animation: fadeOut 3s ease-in-out;
+        animation-fill-mode: forwards; 
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+
+        h2 {
+            font-size: 3rem;
+            margin-bottom: 20px;
+        }
+
+        p {
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+        }
+
+        @keyframes fadeOut {
+            0% {
+                opacity: 1;
+            }
+
+            100% {
+                opacity: 0;
+            }
+        }
     }
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -8,4 +8,6 @@ export const store = reactive({
     apiUrl: 'https://api.themoviedb.org/3/search/multi?api_key=8766d6b6b214fc613264d11608cc2b52&language=it_IT',
     teaserKey: '',
     showTrailer: false,
+    trailerInfo: {},
+    infoHover: false
 })
